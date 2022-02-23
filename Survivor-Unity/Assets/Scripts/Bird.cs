8fb9c31ecc2c844f2976c573293ad77625934fea
arca-ane <99656977+arca-ane@users.noreply.github.com>
2022-02-23 20:35:10 +0530

    Target1.0

--- Survivor-Unity/Assets/Scripts/Bird.cs
+++ Survivor-Unity/Assets/Scripts/Bird.cs
@@ -4,22 +4,84 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-    Rigidbody m_Rigidbody;
-    Vector3 m_Movement;
+    Rigidbody b_Rigidbody;
+    
     public Transform bird;
+    public float b_SpeedValue=75f;
+    public BoxCollider left;
+    public BoxCollider right;
 
+    Vector3 b_Movement;
+    public bool leftFrame=false;
+    public bool rightFrame=true;
+    float b_Speed;
+    public bool lin = true;
+    public bool rin = true;
+
+    float randomForFrame;
+    
     void Start()
     {
-        m_Rigidbody=GetComponent<Rigidbody>();
+        b_Rigidbody=GetComponent<Rigidbody>();
+        b_Speed=b_SpeedValue*Time.deltaTime;
+        randomForFrame=randomGenerate();
     }
 
-    
+
     void FixedUpdate()
     {
-        float horizontal   = Input.GetAxis("Horizontal");
-        
-        m_Movement.Set(horizontal,0f,0f);
-        m_Movement.Normalize();
-        bird.position+=m_Movement; 
+        if(randomForFrame>=0.5)
+        {
+            if(rin)
+            {
+                float x=right.transform.position.x;
+                float y=Random.Range(0,71);
+                b_Movement.Set(x,y,0);
+                bird.position=b_Movement;
+                bird.rotation = Quaternion.Euler(0, 180, 0);
+                rin=false;
+                //lin=false;
+            }
+
+            b_Movement.Set(-b_Speed,0,0);
+            bird.position+=b_Movement;
+        }
+        if(randomForFrame<0.5)
+        {
+            if(lin)
+            {
+                float x=left.transform.position.x;
+                float y=Random.Range(0,30);
+                b_Movement.Set(x,y,0);
+                bird.position=b_Movement;
+                bird.rotation = Quaternion.Euler(0, 0, 0);
+                lin=false;
+                //rin=false;
+            }
+            
+            b_Movement.Set(b_Speed,0,0);
+            bird.position+=b_Movement;
+        }
+        if(rightFrame) 
+        {
+            randomForFrame=randomGenerate();
+            rightFrame=false;
+            lin=true;
+            rin=true;
+        }
+        if(leftFrame)
+        {
+            randomForFrame=randomGenerate();
+            leftFrame=false;
+            rin=true;
+            lin=true;
+        } 
+    }
+
+    float randomGenerate()
+    {
+        float rFrame;
+        rFrame=Random.value;
+        return rFrame;
     }
 }
